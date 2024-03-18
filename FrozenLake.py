6a4aca9f4152a492381e0091625db5193b9174cb
kelvin0108 <kelvin01080108@gmail.com>
2024-03-18 21:56:00 +0800

    add random

--- FrozenLake.py
+++ FrozenLake.py
@@ -1,20 +1,23 @@
+import random
 class Game:
-    def __init__(self, action):
-        self.action = action
+    def __init__(self, is_slippery):
+        
         self.state = (0,0)
+        self.is_slippery = is_slippery
 
     def reset(self):
         self.state = (0, 0)
         return self.state
 
-    def step(self):
-        if 0 <= self.action <= 3 and (0, 0) <= self.state <= (4, 4):
-            if self.action == 0:
-                self.state = (self.state[0] + 1, self.state[1])
-            elif self.action == 1:
-                self.state = (self.state[0], self.state[1] - 1)
-            elif self.action == 2:
-                self.state = (self.state[0] - 1, self.state[1])
-            elif self.action == 3:
-                self.state = (self.state[0], self.state[1] + 1)
+    def step(self, action):
+        assert 0 <= action <= 3, "Error: Action values should fall within the interval [0, 3]."
+        self.action = random.randrange(action - self.is_slippery, action + self.is_slippery, self.is_slippery) 
+        if self.actionaction == 0:
+            self.state = (self.state[0] + 1, self.state[1])
+        elif self.action == 1:
+            self.state = (self.state[0], self.state[1] - 1)
+        elif self.action == 2:
+            self.state = (self.state[0] - 1, self.state[1])
+        elif self.action == 3:
+            self.state = (self.state[0], self.state[1] + 1) 
         return self.state
