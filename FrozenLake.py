5603f8b8af422c5181a32510fc7e5c78e349112e
kelvin0108 <kelvin01080108@gmail.com>
2024-04-03 19:54:16 +0800

    final

--- FrozenLake.py
+++ FrozenLake.py
@@ -4,26 +4,33 @@ import numpy as np
 
 H = np.array([[1, 1], [3, 1], [3, 2], [0, 3]])
 G = np.array([3, 3])
+MAP = [
+    "SFFF",
+    "FHFH",
+    "FFFH",
+    "HFFG"
+    ]
 
 
 class Game:
 
   def __init__(self, is_slippery):
+    assert is_slippery == 0 or is_slippery == 1, "Error: is_slippery values should fall within the interval [0, 1]."
     self.is_slippery = is_slippery
 
   def reset(self):
     self.state = np.array([0, 0])
     self.reward = 0
     self.done = False
-    return self.state, self.reward
+    return self.state
 
   def step(self, action):
     assert 0 <= action <= 3, "Error: Action values should fall within the interval [0, 3]."
-    assert self.done == False, "You have already reached the goal. "
+    assert self.done == False, "The game has ended."
     self.action = random.randrange(action - self.is_slippery,
                                    action + self.is_slippery + 1, 1)
     old_state = self.state.copy()
-    if self.action == 0:
+    if self.action == 0 or self.action == 4:
       self.state[0] += 1
     elif self.action == 1:
       self.state[1] += 1
@@ -31,7 +38,7 @@ class Game:
       self.state[0] -= 1
     elif self.action == 3 or self.action == -1:
       self.state[1] -= 1
-    if (self.state > np.array([4, 4])).any() or (self.state < np.array(
+    if (self.state > np.array([3, 3])).any() or (self.state < np.array(
         [0, 0])).any():
       self.state = old_state
     if (self.state == G).all():
@@ -40,4 +47,4 @@ class Game:
     for i in range(4):
       if (self.state == H[i]).all():
         self.done = True
-    return self.state, self.done
+    return self.state, self.done, self.reward
