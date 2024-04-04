73b0c1531a4d5c371ac7285f023adffd8559ab80
kelvin0108 <kelvin01080108@gmail.com>
2024-04-04 14:09:15 +0800

    final2

--- FrozenLake.py
+++ FrozenLake.py
@@ -1,5 +1,4 @@
 import random
-import tkinter as tk
 import numpy as np
 
 H = np.array([[1, 1], [3, 1], [3, 2], [0, 3]])
@@ -9,42 +8,48 @@ MAP = [
     "FHFH",
     "FFFH",
     "HFFG"
-    ]
+]
 
 
 class Game:
+    def __init__(self, is_slippery):
+        assert is_slippery == 0 or is_slippery == 1, "Error: is_slippery values should fall within the interval [0, 1]."
+        self.is_slippery = is_slippery
+        self.action_space_size = 4
 
-  def __init__(self, is_slippery):
-    assert is_slippery == 0 or is_slippery == 1, "Error: is_slippery values should fall within the interval [0, 1]."
-    self.is_slippery = is_slippery
+    def reset(self):
+        self.state = np.array([0, 0])
+        self.reward = 0
+        self.done = False
+        return self.state
 
-  def reset(self):
-    self.state = np.array([0, 0])
-    self.reward = 0
-    self.done = False
-    return self.state
+    def step(self, action):
+        assert 0 <= action <= self.action_space_size-1, f"Error: Action values should fall within the interval [0, {self.action_space_size-1}]."
+        assert self.done == False, "The game has ended."
+        self.action = random.randrange(action - self.is_slippery,
+                                       action + self.is_slippery + 1, 1)
+        old_state = self.state.copy()
+        if self.action == 0 or self.action == 4:
+            self.state[0] += 1
+        elif self.action == 1:
+            self.state[1] += 1
+        elif self.action == 2:
+            self.state[0] -= 1
+        elif self.action == 3 or self.action == -1:
+            self.state[1] -= 1
+        if (self.state > np.array([3, 3])).any() or (self.state < np.array(
+                [0, 0])).any():
+            self.state = old_state
+        if (self.state == G).all():
+            self.reward += 1
+            self.done = True
+        for i in range(4):
+            if (self.state == H[i]).all():
+                self.done = True
+        return self.state, self.done, self.reward
 
-  def step(self, action):
-    assert 0 <= action <= 3, "Error: Action values should fall within the interval [0, 3]."
-    assert self.done == False, "The game has ended."
-    self.action = random.randrange(action - self.is_slippery,
-                                   action + self.is_slippery + 1, 1)
-    old_state = self.state.copy()
-    if self.action == 0 or self.action == 4:
-      self.state[0] += 1
-    elif self.action == 1:
-      self.state[1] += 1
-    elif self.action == 2:
-      self.state[0] -= 1
-    elif self.action == 3 or self.action == -1:
-      self.state[1] -= 1
-    if (self.state > np.array([3, 3])).any() or (self.state < np.array(
-        [0, 0])).any():
-      self.state = old_state
-    if (self.state == G).all():
-      self.reward += 1
-      self.done = True
-    for i in range(4):
-      if (self.state == H[i]).all():
-        self.done = True
-    return self.state, self.done, self.reward
+    def action_space(self):
+        return np.arange(self.action_space_size)
+
+    def sample_action(self):
+        return np.random.choice(self.action_space(), 1).item()
