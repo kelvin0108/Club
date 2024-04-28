187ea0307cb69e8377362c2ac6c32a30163697ce
kelvin0108 <kelvin01080108@gmail.com>
2024-04-28 13:40:21 +0800

    fix bug: ball penetration

--- Pong.py
+++ Pong.py
@@ -27,6 +27,8 @@ class Game:
     self.opponent_score = 0
     self.done = False
     self.frame = 0
+    if self.render_ == 1:
+      self.render()
     return self.state, self.reward, self.done, self.frame
 
   def reset_ball_and_bar(self):
@@ -46,7 +48,7 @@ class Game:
     return self.ball_angle, self.ball_speed, self.ball_position
 
   def step(self, action):
-    frame_limit = 1500
+    frame_limit = 3150
     detect_range = 45
     self.action = action
     if self.game_mode == "human":
@@ -63,7 +65,6 @@ class Game:
     if self.bar_top + 1 - self.action > 0 and self.bar_top + 1 - self.action + self.bar_length - 1 < 159:
       self.bar_top = self.bar_top + 1 - self.action  # 0:down, 1:no move, 2:up
     self.state[self.bar_top:self.bar_top + self.bar_length, 1] = 3
-    self.update_ball_position()
     self.state[self.opponent_bar_top:self.opponent_bar_top + self.bar_length, 208] = 0
     if self.ball_position[0] >= (208 - detect_range):
       if self.ball_position[1] >= self.opponent_bar_top + (self.bar_length / 2 + 1) and self.opponent_bar_top + 1 + self.bar_length - 1 <= 158:
@@ -75,7 +76,7 @@ class Game:
     elif self.opponent_bar_top > 79 - self.bar_length // 2 + 1:
       self.opponent_bar_top -= 1
     self.state[self.opponent_bar_top:self.opponent_bar_top + self.bar_length, 208] = 2
-
+    self.update_ball_position()
     if self.player_score == 21 or self.opponent_score == 21 or self.frame == frame_limit:
       self.done = True
     self.frame += 1
@@ -88,10 +89,10 @@ class Game:
     new_y = int(self.ball_position[1] +
                 self.ball_speed * math.sin(self.ball_angle))
     if 0<= new_x <= 209:
-      if new_y <= 0 or new_y >= 159 or self.state[new_y, new_x] == 2 or self.state[new_y, new_x] == 3:
+      while new_y <= 0 or new_y >= 159 or new_x <= 1 or new_x >= 208:
         if new_y <= 0 or new_y >= 159:
           self.ball_angle = math.pi * 2 - self.ball_angle
-        elif self.state[new_y, new_x] == 2 or self.state[new_y, new_x] == 3:
+        elif new_x <= 1 or new_x >= 208:
           self.ball_angle = (math.pi * 3 - self.ball_angle) % (2 * math.pi)
         new_x = int(new_x + self.ball_speed * math.cos(self.ball_angle))
         new_y = int(new_y + self.ball_speed * math.sin(self.ball_angle))
@@ -126,14 +127,15 @@ class Game:
       self.angle_text = self.ax.text(0.5, 1.01, f"Angle: {self.ball_angle:.2f}", transform=self.ax.transAxes,
                                       ha="center")  
     if self.frame_text:
-            self.frame_text.set_text(f"Frame: {self.frame}")  
+      self.frame_text.set_text(f"Frame: {self.frame}")  
     else:
-        self.frame_text = self.ax.text(0.5, 0.01, f"Frame: {self.frame}", transform=self.ax.transAxes,
-                                        ha="center")  
+      self.frame_text = self.ax.text(0.5, 0.01, f"Frame: {self.frame}", transform=self.ax.transAxes,
+                                      ha="center")  
     if self.player_score_text:
       self.player_score_text.set_text(f"Player Score: {self.player_score}")  
+    else:
       self.player_score_text = self.ax.text(0.01, 0.95, f"Player Score: {self.player_score}", transform=self.ax.transAxes,
-                                              ha="left") 
+                                              ha="left")  
     if self.opponent_score_text:
       self.opponent_score_text.set_text(f"Opponent Score: {self.opponent_score}")  
     else:
@@ -141,10 +143,3 @@ class Game:
                                                 ha="right")             
     self.fig.canvas.draw()  
     plt.pause(0.001)  
-import time
-game = Game("human")
-game.reset(1)
-while True:
-  game.step(0)
-  time.sleep(0.001)
-
