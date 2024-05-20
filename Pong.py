57bd80f2305d422b47534f3fb0593bf672ab49ed
kelvin0108 <kelvin01080108@gmail.com>
2024-05-20 22:27:01 +0800

    fix penetrate

--- Pong.py
+++ Pong.py
@@ -6,18 +6,24 @@ import pygame
 import sys
 from pygame.locals import *
 import time
+from pynput.keyboard import Controller, Key
 
 class Game:
     def __init__(self, game_mode):
         self.game_mode = game_mode
         self.action_space_size = 3
-        self.bar_length = 30
+        self.bar_length = 40
         self.frame_limit = 8000
         self.hide_info = False
         self.skip_step = False
+        self.keyboard = Controller()
+
 
     def reset(self, render):
-        self.pause = True
+        if self.game_mode == "human":
+            self.pause = True
+        else:
+            self.pause = False
         self.render_ = render
         self.reset_ball_and_bar()
         self.player_score = 0
@@ -42,37 +48,41 @@ class Game:
         self.ball_position = [random.randrange(104, 106), random.randrange(79, 81)]
         self.state[self.ball_position[1], self.ball_position[0]] = 1
         self.reward = 0
+        self.vx = int(self.ball_speed * math.cos(self.ball_angle))
+        self.vy = int(self.ball_speed * math.sin(self.ball_angle))
         return self.ball_angle, self.ball_speed, self.ball_position
 
     def step(self, action): 
         detect_range = 45
         self.action = action
+        bar_speed = 1
         self.stop_one_sec = False
         self.skip_step = False
         if self.game_mode == "human":
-            key = keyboard.read_key()
-            while key != "down" and key != "space" and key != "up" and key != "enter" and key != "shift" and key != "right shift" and key != "esc":
+            if keyboard.is_pressed("up") or keyboard.is_pressed("down") or keyboard.is_pressed("enter") or keyboard.is_pressed("shift") or keyboard.is_pressed("esc") or keyboard.is_pressed("right shift"):
                 key = keyboard.read_key()
-            if key == "esc":
-                sys.exit()
-            elif key == "shift" or key == "right shift":
-                if self.hide_info == False:    
-                    self.hide_info = True
-                else:
-                    self.hide_info = False
-                self.render()
-                self.skip_step = True
-                time.sleep(0.5)
-            if key == "down":
-                self.action = 0
-            elif key == "space":
+                while key != "down" and key != "up" and key != "enter" and key != "shift" and key != "right shift" and key != "esc":
+                    key = keyboard.read_key()
+                if key == "esc":
+                    sys.exit()
+                elif key == "shift" or key == "right shift":
+                    if self.hide_info == False:    
+                        self.hide_info = True
+                    else:
+                        self.hide_info = False
+                    self.render()
+                    self.skip_step = True
+                    time.sleep(0.5)
+                if key == "down":
+                    self.action = 0
+                elif key == "up":
+                    self.action = 2    
+                elif key == "enter":
+                    self.pause = True
+                    self.render()
+                    self.stop_one_sec = True
+            else:
                 self.action = 1
-            elif key == "up":
-                self.action = 2    
-            elif key == "enter":
-                self.pause = True
-                self.render()
-                self.stop_one_sec = True
 
         elif self.game_mode == "ai":
             assert 0 <= action <= self.action_space_size - 1, f"Error: Action values should fall within the interval [0, {self.action_space_size-1}]."
@@ -86,18 +96,18 @@ class Game:
             if self.pause == False:
                 self.state[self.bar_top:self.bar_top + self.bar_length, 1] = 0
                 if self.bar_top + 1 - self.action > 0 and self.bar_top + 1 - self.action + self.bar_length - 1 < 159:
-                    self.bar_top = self.bar_top + 1 - self.action  # 0:down, 1:no move, 2:up
+                    self.bar_top = self.bar_top + (1 - self.action) * bar_speed  # 0:down, 1:no move, 2:up
                 self.state[self.bar_top:self.bar_top + self.bar_length, 1] = 3
                 self.state[self.opponent_bar_top:self.opponent_bar_top + self.bar_length, 208] = 0
                 if self.ball_position[0] >= (208 - detect_range):
                     if self.ball_position[1] >= self.opponent_bar_top + (self.bar_length / 2 + 1) and self.opponent_bar_top + 1 + self.bar_length - 1 <= 158:
-                        self.opponent_bar_top += 1  # go down
+                        self.opponent_bar_top += bar_speed  # go down
                     elif self.ball_position[1] <= self.opponent_bar_top + (self.bar_length / 2) and self.opponent_bar_top >= 1:
-                        self.opponent_bar_top -= 1  # go up
+                        self.opponent_bar_top -= bar_speed  # go up
                 elif self.opponent_bar_top < 79 - self.bar_length // 2 + 1:
-                    self.opponent_bar_top += 1
+                    self.opponent_bar_top += bar_speed      #go back
                 elif self.opponent_bar_top > 79 - self.bar_length // 2 + 1:
-                    self.opponent_bar_top -= 1
+                    self.opponent_bar_top -= bar_speed      #go back
                 self.state[self.opponent_bar_top:self.opponent_bar_top + self.bar_length, 208] = 2
                 self.update_ball_position()
                 if self.player_score == 21 or self.opponent_score == 21 or self.frame == self.frame_limit:
@@ -105,21 +115,30 @@ class Game:
                 self.frame += 1
                 if self.render_ == 1:
                     self.render()
+                    if self.game_mode == "ai":
+                        clock = pygame.time.Clock()
+                        pygame.display.update()
+                        clock.tick(120)
             info = {"player": self.player_score, "opponent": self.opponent_score}
             return self.state.reshape((1, 210, 160)), self.reward, self.done, info
 
     def update_ball_position(self):
-        new_x = int(self.ball_position[0] + self.ball_speed * math.cos(self.ball_angle))
-        new_y = int(self.ball_position[1] + self.ball_speed * math.sin(self.ball_angle))
+        new_x = int(self.ball_position[0] + self.vx)
+        new_y = int(self.ball_position[1] + self.vy)
         if 0 <= new_x <= 209 and 6 <= self.ball_position[0] <= 203:
             while new_y <= 0 or new_y >= 159 or (new_x <= 6 and self.bar_top <= new_y <= self.bar_top + self.bar_length - 1) or (new_x >= 203 and self.opponent_bar_top <= new_y <= self.opponent_bar_top + self.bar_length - 1):
                 if new_y <= 0 or new_y >= 159:
-                    self.ball_angle = math.pi * 2 - self.ball_angle
+                    self.vy = -self.vy
+                    new_y = self.ball_position[1]
+                    
+                    
                 elif (new_x <= 6 and self.bar_top <= new_y <= self.bar_top + self.bar_length - 1) or (new_x >= 203 and self.opponent_bar_top <= new_y <= self.opponent_bar_top + self.bar_length - 1):  # 板子打到球
-                    self.ball_angle = (math.pi * 3 - self.ball_angle) % (2 * math.pi)
+                    self.vx = -self.vx
+                    new_x = self.ball_position[0]
                     self.ball_speed = 3
-                new_x = int(new_x + self.ball_speed * math.cos(self.ball_angle))
-                new_y = int(new_y + self.ball_speed * math.sin(self.ball_angle))
+                    
+                new_x = int(new_x + self.vx)
+                new_y = int(new_y + self.vy)
             self.state[self.ball_position[1], self.ball_position[0]] = 0
         self.ball_position = [new_x, new_y]
         if new_x <= 0:
@@ -200,32 +219,29 @@ class Game:
 
 
 
-    def run_game(self):
-        while True:
-            if self.pause == False:
-                self.step(4)
-                print(self.hide_info)
-                pygame.display.update()
-                clock.tick(120)
-                if self.stop_one_sec:
-                    time.sleep(0.5)
-                    
-
-            else:
-                key = keyboard.read_key()
-                if key == "enter":
-                    self.pause = False
-                    self.render()
+    def run_game_for_human(self):
+        if self.game_mode =="human":
+            clock = pygame.time.Clock()
+            while True:
+                if self.pause == False:
+                    self.step(4)
                     pygame.display.update()
                     clock.tick(120)
-                    time.sleep(0.5)
-                elif key == "esc":
-                    sys.exit()
+                    if self.stop_one_sec or self.skip_step:
+                        time.sleep(0.5)
+                else:
+                    key = keyboard.read_key()
+                    if key == "enter":
+                        self.pause = False
+                        self.render()
+                        pygame.display.update()
+                        clock.tick(120)
+                        time.sleep(0.5)
+                    elif key == "esc":
+                        sys.exit()
             
 
 
-clock = pygame.time.Clock()
 game = Game("human")
 game.reset(1)
-game.run_game()
-
+game.run_game_for_human()
