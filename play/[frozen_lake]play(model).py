1125c74c292f97a7483651889c98842c04d9c5ce
greedjesse <67365952+greedjesse@users.noreply.github.com>
2024-05-29 19:03:40 +0800

    Trained Models and Play

---
+++ play/[frozen_lake]play(model).py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+
+from environments import frozen_lake
+
+env = frozen_lake.Game(1, 150)
+
+net = nn.Sequential(
+    nn.Linear(16, 200),
+    nn.ReLU(),
+    nn.Linear(200, 100),
+    nn.ReLU(),
+    nn.Linear(100, 4)
+)
+net.load_state_dict(torch.load("../Trained_Models/FrozenLake(96%) (16, 200, 100, 4)"))
+
+state = env.reset()
+
+print("Initial:")
+
+total_reward = 0
+while True:
+    print(env.show_map(), end="\r")
+
+    action = net(torch.FloatTensor(state)).max(0)[1].item()
+    state, done, reward = env.step(action)
+
+    if action == 0:
+        display_action = "right"
+    elif action == 1:
+        display_action = "down"
+    elif action == 2:
+        display_action = "left"
+    elif action == 3:
+        display_action = "up"
+    print("Action:", display_action, end="\n\n")
+    total_reward += reward
+    if done:
+        break
+
+print(total_reward)
+
